d03caf03e7950d430b27131a52e86f4580a56f44
William Diaz <williamefrain1994@gmail.com>
2022-03-15 18:01:21 -0600

    go

---
+++ ClasesGenerales/src/main/java/Entidades/Estudiante.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+/**
+ *
+ * @author William
+ */
+public class Estudiante extends Materia{
+    
+    String Nombre;
+    String Apellido;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+    
+}
